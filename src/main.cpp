6ca5412ceee85e5e594e9d22ab9996dd03d6dc48
Joel Carter <carter3689@gmail.com>
2017-09-24 15:18:12 -0500

    First Commit for project

--- src/main.cpp
+++ src/main.cpp
@@ -53,14 +53,14 @@ int main() {
 
 	// Read position data
 	vector<control_s> position_meas;
-	if (!read_control_data("data/control_data.txt", position_meas)) {
+	if (!read_control_data("../data/control_data.txt", position_meas)) {
 		cout << "Error: Could not open position/control measurement file" << endl;
 		return -1;
 	}
 
 	// Read ground truth data
 	vector<ground_truth> gt;
-	if (!read_gt_data("data/gt_data.txt", gt)) {
+	if (!read_gt_data("../data/gt_data.txt", gt)) {
 		cout << "Error: Could not open ground truth data file" << endl;
 		return -1;
 	}
